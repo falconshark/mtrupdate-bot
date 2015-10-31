da746819461b1a444762419a34a74c4ea5e0a953
Sardo <sardo.ip@sardo.work>
2015-11-01 00:26:57 +0800

    Fix syntax error and rewrite get bot message part

--- bot.js
+++ bot.js
@@ -3,29 +3,36 @@ var twitter = require('twitter');
 var aws = require('aws-sdk');
 var nconf = require('nconf');
 var log4js = require('log4js');
+var database = require(__dirname + '/lib/database');
 
 nconf.file('config', __dirname + '/config/config.json');
 
 var logger = log4js.getLogger('BOT-LOG');
 
 var botToken = nconf.get('telegram').bot_token;
-var bot = new TelegramBot(botToken, {
+var bot = new telegramBot(botToken, {
 	polling: true
 });
 
 var awsAccessKeyId = nconf.get('database')['aws'].access_key_id;
 var awsSecretAccessKey = nconf.get('database')['aws'].secret_access_key;
 
-aws.config.update{accessKeyId: awsAccessKeyId, secretAccessKey: awsSecretAccessKey, region: 'ap-southeast-1'};
+aws.config.update({
+	accessKeyId: awsAccessKeyId,
+	secretAccessKey: awsSecretAccessKey,
+	region: 'ap-southeast-1'
+});
 
 var dyDb = new aws.DynamoDB();
 
-bot.onText('/register', function(msg, match) {
+bot.on('message', function(msg) {
 
-	var userId = msg.from.id;
+	if (msg.text === '/register') {
 
-	updateUserList(dyDb, userId, function(err, result){
+		var userId = msg.chat.id;
 
+		database.updateUserList(dyDb, userId, function(err, result) {
 
-	});
-}
+		});
+	}
+});
