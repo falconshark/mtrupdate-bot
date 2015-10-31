a0e63c6a56bbab1bef665f31bc282d67235f98b5
Sardo <sardo.ip@sardo.work>
2015-10-31 23:52:18 +0800

    Will get telegram and aws api key from config file

--- bot.js
+++ bot.js
@@ -4,6 +4,14 @@ var aws = require('aws-sdk');
 var nconf = require('nconf');
 var log4js = require('log4js');
 
-function updateUserList(){
+nconf.file('config', __dirname + '/config/config.json');
 
-}
+var botToken = nconf.get('telegram').bot_token;
+var bot = new TelegramBot(botToken, {
+	polling: true
+});
+
+var awsAccessKeyId = nconf.get('database')['aws'].access_key_id;
+var awsSecretAccessKey = nconf.get('database')['aws'].secret_access_key;
+
+aws.config.update{accessKeyId: awsAccessKeyId, secretAccessKey: awsSecretAccessKey, region: 'ap-southeast-1'};
