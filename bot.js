d6242d5522ad7d04f3913c8e8cc969cb18135491
Sardo <sardo.ip@sardo.work>
2015-10-31 22:59:48 +0800

    Require aws idk and add updateUserList function

--- bot.js
+++ bot.js
@@ -1,4 +1,9 @@
 var telegramBot = require('node-telegram-bot-api');
 var twitter = require('twitter');
+var aws = require('aws-sdk');
 var nconf = require('nconf');
 var log4js = require('log4js');
+
+function updateUserList(){
+
+}
